1e6aed9581043a1d9fd33e4fcf65a8b44e7f8f5a
Omidcr77 <100779820+Omidcr77@users.noreply.github.com>
2023-06-12 17:53:02 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('answer-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const answerInput = document.getElementById('answer');
+  const answer = answerInput.value.trim().toLowerCase();
+
+  if (answer === 'i love you') {
+    document.getElementById('result').textContent = 'You know what I love you too ';
+    document.getElementById('result').classList.remove('hidden');
+    answerInput.disabled = true;
+  } else {
+    document.getElementById('result').textContent = 'Incorrect answer. Please try again.';
+    document.getElementById('result').classList.remove('hidden');
+  }
+});
